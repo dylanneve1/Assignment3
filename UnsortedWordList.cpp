895585801dbc0f5cd7b250f94b04f0e7b586bb3e
Dylan Neve <dylanneve1@gmail.com>
2023-11-02 10:52:56 +0000

    UnsortedWordList: Populate functions

--- UnsortedWordList.cpp
+++ UnsortedWordList.cpp
@@ -1,3 +1,70 @@
 //
 // Created by neved on 02/11/2023.
 //
+
+#include "UnsortedWordList.h"
+
+#include <iostream>
+
+using namespace std;
+
+UnsortedWordList::UnsortedWordList() {
+}
+
+UnsortedWordList::~UnsortedWordList() {
+}
+
+bool UnsortedWordList::IsEmpty() const {
+    bool ret;
+    return ret;
+}
+
+int UnsortedWordList::Length() const {
+    int ret;
+    return ret;
+}
+
+Node *UnsortedWordList::Front() const {
+    Node *ret;
+    return ret;
+}
+
+Node *UnsortedWordList::Back() const {
+    Node *ret;
+    return ret;
+}
+
+bool UnsortedWordList::Contains(std::string word) {
+    bool ret;
+    return ret;
+}
+
+Node *UnsortedWordList::FindWord(std::string word) {
+    Node *ret;
+    return ret;
+}
+
+string UnsortedWordList::MaxWord() const {
+    string ret;
+    return ret;
+}
+
+int UnsortedWordList::MaxCount() const {
+    int ret;
+    return ret;
+}
+
+void UnsortedWordList::Print() const {
+}
+
+void UnsortedWordList::CountWord(std::string word) {
+}
+
+void UnsortedWordList::InsertFirst(std::string word) {
+}
+
+void UnsortedWordList::DeleteAll() {
+}
+
+void UnsortedWordList::DeleteWord(std::string word) {
+}
