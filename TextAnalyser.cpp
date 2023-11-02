43d9d4ca15e2f54c9fea19cab8e1c33795dec515
Dylan Neve <dylanneve1@gmail.com>
2023-11-02 10:36:25 +0000

    TextAnalyser: Import sample fragment

--- TextAnalyser.cpp
+++ TextAnalyser.cpp
@@ -1,3 +1,26 @@
 //
 // Created by neved on 02/11/2023.
 //
+
+#include "TextAnalyser.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void TextAnalyser::ReadFile(string filename) {
+    cout << "TextAnalyser::ReadFile, filename='" << filename << "'." << endl;
+    ifstream file;
+    file.open(filename);
+    if (!file.is_open()) {
+        cout << "unable to open file with filename='" << filename << "'." << endl;
+        return;
+    }
+    string word;
+    // TODO if necessary add code here
+    while (file >> word) {
+        // TODO replace with code that handles each word
+        cout << "word:" << word << endl;
+    }
+    // TODO if necessary add code here
+}
